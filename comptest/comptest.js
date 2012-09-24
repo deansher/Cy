974c48a1bdf890e38e92679c84710f83f813bac0
Dean Thompson <dthompson@linkedin.com>
2012-09-24 07:49:47 -0700

    First breaking version of comptest compiler test.

---
+++ comptest/comptest.js
@@ -0,0 +1,11 @@
+var mocha = require ('mocha')
+var comptest = require('__staq/staq/test/comptest/0.0.1')
+
+describe('Staq compiler output', function(){
+  
+  describe('generated module', function(){
+    it('should have __staq_source_md5', function(){
+      comptest.__staq_source_md5.should.b.a('string');
+    })
+  })
+})
