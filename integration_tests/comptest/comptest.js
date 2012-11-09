31bfaae46025023b8a2525cc4acbdeac363117b6
Dean Thompson <dthompson@linkedin.com>
2012-11-09 13:39:01 -0800

    Added license block to comptest.js

--- integration_tests/comptest/comptest.js
+++ integration_tests/comptest/comptest.js
@@ -1,3 +1,13 @@
+/***** BEGIN LICENSE BLOCK *****
+ *
+ * Copyright (C) 2012 by Dean Thompson.  All Rights Reserved.
+ *
+ * The contents of this file are subject to the Apache 2 license:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * ***** END LICENSE BLOCK *****/
+
+
 var mocha = require ('mocha')
 var comptest = require('__cy/cy/test/comptest/0.0.1')
 
