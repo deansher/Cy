ddaba740062efae2b7e97e6d13841793bc14c10e
Dean Thompson <dthompson@linkedin.com>
2012-12-10 05:36:39 -0800

    Tiny bit of exploratory work on Compiler.hs.

--- haskell/Cy/Compiler.hs
+++ haskell/Cy/Compiler.hs
@@ -10,39 +10,116 @@
 {-# LANGUAGE FlexibleInstances, BangPatterns, OverloadedStrings #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
+-- For now, the Cy compiler is a command-line tool that loads all needed state, incrementally
+-- rebuilds all modified source under a specified Cy root, and exits.  The compiler will become a
+-- daemon that watches for filesystem changes and incrementally rebuilds as needed.
+
 module Cy.Compiler (
   main
 ) where
 
 import System.Directory (getCurrentDirectory)
-import Control.Monad (forM_, filterM)
+import Control.Monad (mapM)
 import Control.Applicative ((<$>))
+
+import qualified Data.Sequence as Seq
+import Data.Sequence (Seq)
+
+import qualified Data.Strict.Maybe as SM
+
+-- A RootState represents the known state (partly definitional and partly cached) of a Cy root directory.
+data RootState = RootState {
+    rstateDir :: !FilePath 
+  , rstatePackages :: !(Seq PackageState)
+}
+
+initRootState :: FilePath -> RootState
+initRootState rootDir = RootState { rstateDir = rootDir, rstatePackages = SM.Nothing }
+
+-- A PackageState represents the cached state of a Cy package.
+data PackageState = PackageState {
+    pstateName :: !PackageName
+  , pstateModules :: !(Seq ModuleState)
+}
+
+data ModuleState = ModuleState {
+    mstateName :: !ModuleName
+}
+
+-- Options are the compiler's command-line options and (potentially) environment variables.
+data Options = Options {
+    optionsErrorMessages :: !(Seq String)
+  , optionsRootDir :: !FilePath
+  , optionsOtherRootDirs :: FilePath
+}
+
+optionsHasErrors :: Options -> Bool
+optionsHasErrors options = !Seq.empty $ optionsErrorMessages options
 
 main :: IO ()
 main = do
-  currentHome <- findCurrentHomeDir
-  homeDirs <- findHomeDirs currentHome
-  currentHomeModules <- findModules currentHome
-  dirtyCurrentHomeModules <- filterM moduleIsDirty currentHomeModules 
-  forM_ dirtyCurrentHomeModules $ \dirtyModule -> do
-    rebuild dirtyModule
+  options <- parseOptions
+  if optionsHasErrors
+  then printUsage
+  else rootState <- makeRootState $ optionsRootDir options
+       otherRootStates <- mapM makeRootState $ optionsOtherRootDirs options
+       incrementallyRebuildRoot rootState otherRootStates options
+       where makeRootState = updateRootStateFromFilesystem . initRootState
 
-data Module = Module {
-    moduleName :: String
-  }
+updateRootStateFromFilesystem :: RootState -> IO RootState
 
-findCurrentHomeDir :: IO FilePath
-findCurrentHomeDir = getCurrentDirectory
+incrementallyRebuildRoot :: RootState -> [RootState] -> Options -> IO ()
+incrementallyRebuildRoot rstate otherRStates options = do
+  rebuiltPackages <- mapM incrementallyRebuildPackage $ rstatePackages rstate
+  rstate { rstatePackages = rebuiltPackages }
 
-findHomeDirs :: FilePath -> IO [FilePath]
-findHomeDirs currentHome = return [currentHome]
+ ---------------------------------------------------------------------
+ -- Root exception type for all compiler exceptions
 
-findModules :: FilePath -> IO [Module]
-findModules homeDir = return []
+ data SomeCompilerException = forall e . Exception e => SomeCompilerException e
+     deriving Typeable
 
-moduleIsDirty :: Module -> IO Bool
-moduleIsDirty m = return True
+ instance Show SomeCompilerException where
+     show (SomeCompilerException e) = show e
 
-rebuild :: Module -> IO ()
-rebuild m = do
-  putStrLn $ "Rebuilt " ++ moduleName m
+ instance Exception SomeCompilerException
+
+ compilerExceptionToException :: Exception e => e -> SomeException
+ compilerExceptionToException = toException . SomeCompilerException
+
+ compilerExceptionFromException :: Exception e => SomeException -> Maybe e
+ compilerExceptionFromException x = do
+     SomeCompilerException a <- fromException x
+     cast a
+
+ ---------------------------------------------------------------------
+ -- Subhierarchy for exceptions in the compiler frontend
+
+ data SomeFrontendException = forall e . Exception e => SomeFrontendException e
+     deriving Typeable
+
+ instance Show SomeFrontendException where
+     show (SomeFrontendException e) = show e
+
+ instance Exception SomeFrontendException where
+     toException = compilerExceptionToException
+     fromException = compilerExceptionFromException
+
+ frontendExceptionToException :: Exception e => e -> SomeException
+ frontendExceptionToException = toException . SomeFrontendException
+
+ frontendExceptionFromException :: Exception e => SomeException -> Maybe e
+ frontendExceptionFromException x = do
+     SomeFrontendException a <- fromException x
+     cast a
+
+ ---------------------------------------------------------------------
+ -- Specific frontend exceptions
+
+ data IllegalDirectoryStructure = IllegalDirectoryStructure
+     deriving (Typeable, Show)
+
+ instance Exception IllegalDirectoryStructure where
+     toException   = frontendExceptionToException
+     fromException = frontendExceptionFromException
+
